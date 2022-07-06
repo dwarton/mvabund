1859d1d5f1bed52bb1a4c2bedcd09b7d11f26d1d
David Warton <david.warton@unsw.edu.au>
2022-07-06 13:03:46 +1000

    block.to.boot fixes
    also correcting spider help file

--- R/manyglm.R
+++ R/manyglm.R
@@ -127,8 +127,8 @@ if(composition==TRUE)
     #DW edits, 1/2/22, so works when data is a list (like Code Box 14.6 of Eco-Stats text)
     whichResponse=which(names(data)==names(mf[1]))
     if(inherits(data,"data.frame")==FALSE)
-      data2 = as.data.frame(data[-whichResponse])
-    dat = data.frame(c(Y), data2[rep(1:N,p),])
+      data = as.data.frame(data[-whichResponse])
+    dat = data.frame(c(Y), data[rep(1:N,p),])
     names(dat)[1] = names(mf)[1]  #match name to original object
     whichResponse=1
     # end DW edits
