1703e7a3df9d27635e03791451ec71188945b6ae
David Warton <david.warton@unsw.edu.au>
2023-09-08 15:43:31 +1000

    fix manygam
    fix error in model.frame call for gam objects in manyany, using mgcv hack (interpret.gam..$fake.formula)

--- R/manyany.R
+++ R/manyany.R
@@ -34,10 +34,19 @@ manyany = function(formula, fn, family="negative.binomial", data=NULL, compositi
   
   
   tol=1.e-8 #for truncation of linear predictor
-  if(missing(data)) # Only coerce to model frame if not specified by the user.
-    mf =model.frame(formula, parent.frame())
+  
+  #get the formula for extracting the data frame
+  if(fn=="gam")
+    mFormula = mgcv::interpret.gam(formula)$fake.formula
   else
-    mf = model.frame(formula,data=data)
+    mFormula = formula
+    
+  if(missing(data)) # Only coerce to model frame if not specified by the user.
+  {
+    mf =model.frame(mFormula, parent.frame())
+  }
+  else
+    mf = model.frame(mFormula,data=data)
 
   # get response and its dimensions
   yMat = model.response(mf)
